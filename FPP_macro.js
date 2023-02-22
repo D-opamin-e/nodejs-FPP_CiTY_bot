388065ab41d78c21adcb68523b8f465fcb0ab5b2
Sang Jae Heo <66764989+D-opamin-e@users.noreply.github.com>
2023-02-23 04:27:41 +0900

    Update FPP_macro.js

--- FPP_macro.js
+++ FPP_macro.js
@@ -20,7 +20,7 @@ var connection = mysql.createConnection({
   host : "localhost",
   port : 3306,
   user : "root",
-  password : "dopamine2312@@",
+  password : "구버전",
   database : "sunam_test",
 });
 
